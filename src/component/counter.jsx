45a3bdc02a815a5cf63c31617519d3543117b691
Doublefffccccfff <dixit.ankit917@gmail.com>
2025-05-27 12:05:29 +0530

    1

---
+++ src/component/counter.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      lastName: ""
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <h4>Count: {this.state.count}</h4>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
